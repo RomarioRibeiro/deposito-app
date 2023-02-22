99eeb5fcdcd19a466d7b2b34f701208158788ba1
RomarioRibeiro <romaryjane@hotmail.com>
2023-02-22 17:33:56 -0300

    Atualizando aplicativo

--- src/app/core/lancamento.model.ts
+++ src/app/core/lancamento.model.ts
@@ -1,7 +1,7 @@
 export class Lancamento {
   codigo?: number;
   descricao?: string;
-  data_fiado = new  Date();
+  data_fiado?: Date;
   data_pagamento?: Date;
   valor?: number;
   observacao?: string;
